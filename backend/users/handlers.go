9fcbff6e358dc337ee15079c764cb0fa52a0be73
theluqmn <theluqmn@outlook.com>
2026-05-28 09:27:06 +0800

    added input validation for signups

--- backend/users/handlers.go
+++ backend/users/handlers.go
@@ -1,30 +1,37 @@
 package users
 
 import (
+	"main/util"
 	"strings"
 
 	"github.com/labstack/echo/v4"
 )
 
 func SignUpHandler(c echo.Context) error {
-	var id string
+	var id string // required, between 4 and 64 characters
 	q1 := strings.ToLower(c.QueryParam("id"))
 	if q1 == "" {
 		return c.JSON(400, "an ID is required!")
+	} else if !util.InputLongEnough(q1, 4, 64) {
+		return c.JSON(400, "ID must be between 4 and 16 characters long")
 	}
 	id = q1
 
-	var name string
+	var name string // optional, defaults to the ID, and is between 2 and 96 characters
 	q2 := c.QueryParam("name")
 	if q2 == "" {
-		q2 = id // defaults to the ID if no name is given
+		q2 = id
+	} else if !util.InputLongEnough(q2, 2, 96) {
+		return c.JSON(400, "name must be between 4 and 32 characters long")
 	}
 	name = q2
 
-	var password string
+	var password string // required, between 8 and 64 characters
 	q3 := c.QueryParam("password")
 	if q3 == "" {
 		return c.JSON(400, "a password is required!")
+	} else if !util.InputLongEnough(q3, 8, 64) {
+		return c.JSON(400, "password must be between 8 and 32 characters long")
 	}
 	password = q3
 
---
+++ backend/util/functions.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"os"
+	"os/exec"
+)
+
+// checks if input is within specified min/max length
+func InputLongEnough(input string, min int, max int) bool {
+	length := len(input)
+	return length >= min && length <= max
+}
+
+// clears the terminal
+func Clear() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
